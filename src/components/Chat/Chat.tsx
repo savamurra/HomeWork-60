987cc73321e521298c0982fd2918d93b81f65bd3
Sanzhar Beishekeev <franki-55@mail.ru>
2024-10-13 18:06:15 +0600

    Добавил состояние в которое будет приходить дата последнего сообщения и исходя от него будет работать интервал для получения новых сообщений

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react';
 import { Message } from '../../.types';
 import './Chat.css'
+import { ListGroup } from 'react-bootstrap';
 
 const Chat = () => {
+  const [lastDataTime, setLastDataTime] = useState<string>('');
   const [chat, setChat] = useState<Message[]>([
     {
       message: '',
@@ -12,31 +14,62 @@ const Chat = () => {
     }
   ]);
 
+
   const fetchT = async (url: string) => {
-    const message = await fetch(url);
-    if (message.ok) {
-      const mes = await message.json();
-      setChat(mes);
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const allMessages = await response.json();
+        if (allMessages.length > 0) {
+          const newMessages = allMessages.filter((msg: Message) => {
+            for (let i = 0; i < chat.length; i++) {
+              if (chat[i]._id === msg._id) {
+                return false;
+              }
+            }
+            return true;
+          })
+          if (newMessages.length > 0) {
+            setLastDataTime(newMessages[newMessages.length - 1].datetime);
+            setChat((prevChat) => [...prevChat, ...newMessages]);
+          }
+        }
+      }
+    } catch (error) {
+      alert(error);
     }
-  }
+
+  };
 
   useEffect(() => {
-  void fetchT('http://146.185.154.90:8000/messages')
-  }, []);
+    void fetchT('http://146.185.154.90:8000/messages');
 
-  console.log(chat)
+    const interval = setInterval(() => {
+      if (lastDataTime) {
+        void fetchT(`http://146.185.154.90:8000/messages?datetime=${lastDataTime}`);
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, [fetchT, lastDataTime]);
+
+
 
   return (
-    <div className='chatWindow'>
+    <div className="chatWindow">
       <h1>Mobile Chat</h1>
-
-      {chat.map(item => (
-        <div key={item._id}>
-          <h3>{item.author}</h3>
-          <p>{item.message}</p>
-          <p>{new Date(item.datetime).toLocaleString()}</p>
-        </div>
-      ))}
+      <ListGroup>
+        {chat.slice().reverse().map(item => (
+          <div key={item._id + crypto.randomUUID()}>
+            <ListGroup.Item>
+              <strong>{item.author}</strong>
+            </ListGroup.Item>
+            <ListGroup.Item className={'d-flex justify-content-between'}>
+              {item.message} <span>{new Date(item.datetime).toLocaleTimeString()}</span>
+            </ListGroup.Item>
+          </div>
+        ))}
+      </ListGroup>
     </div>
   );
 };
